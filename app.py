666daa999b488adcaae4053c1e593fd4ad196851
Jorge Gomes <jorge.gomes@vost.pt>
2021-04-26 17:33:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 
 # Read CSV file 
