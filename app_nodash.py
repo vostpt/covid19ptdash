3b7bfb3f81c26a898e169ad9c5dc18d9eadb41f8
Jorge Gomes <jorge.gomes@vost.pt>
2021-04-26 18:21:51 +0100

    Create app_nodash.py

---
+++ app_nodash.py
@@ -0,0 +1,23 @@
+
+import pandas as pd 
+import plotly.express as px 
+
+
+
+
+
+
+# Read CSV file 
+
+df = pd.read_csv("covid19pt_data.csv")
+
+## set the index as the data column
+## all of the remaining columns are now of the same int64 type
+df = df.set_index('data')
+
+# Plot
+fig = px.bar(df)
+
+fig.show()
+
+
