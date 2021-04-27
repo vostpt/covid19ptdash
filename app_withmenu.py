f78bed7a3d9aeb718be0feb934fff30dc548aaa4
Jorge Gomes <jorge.gomes@vost.pt>
2021-04-27 18:38:49 +0100

    Update

--- app_withmenu.py
+++ app_withmenu.py
@@ -22,7 +22,7 @@ fig = go.Figure()
 
 #set up ONE trace
 fig.add_trace(go.Bar(x=df.index,
-						 y=df[df.columns[0]],
+ y=df[df.columns[0]],
 						 # marker_color = colors,
 						 visible=True))
 
@@ -32,14 +32,14 @@ buttons = []
 
 # button with one option for each column 
 for col in df.columns:
-    buttons.append(dict(method='restyle',
-                        label=col,
-                        visible=True,
-                        args=[{'y':[df[col]],
-                               'x':[df.index],
-                               'type':'bar'}, [0]],
-                        )
-                  )
+  buttons.append(dict(method='restyle',
+    label=col,
+    visible=True,
+    args=[{'y':[df[col]],
+    'x':[df.index],
+    'type':'bar'}, [0]],
+    )
+  )
 
 # adjustments to the updatemenus
 
@@ -57,7 +57,17 @@ fig.update_layout(showlegend=False, updatemenus=updatemenu)
 fig.update_layout(
   template='plotly_dark',
   margin=dict(r=10, t=25, b=40, l=60)
-)
+  )
+
+
+def customLegend(fig, nameSwap):
+    for i, dat in enumerate(fig.data):
+        for elem in dat:
+            if elem == 'name':
+                fig.data[i].name = nameSwap[fig.data[i].name]
+    return(fig)
+
+fig = customLegend(fig=fig, nameSwap = {'casos_novos_t': 'Novos Casos Total', 'casos_novos_d':'Novos Casos Diários'})
 
 # Show Graphics
 fig.show()
